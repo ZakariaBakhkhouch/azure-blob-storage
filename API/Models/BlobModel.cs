d1eccc8062c650f2c4bb3b50f934cc575fc141fe
ZAKARIA BAKHKHOUCH <zakabakho@gmail.com>
2024-11-18 14:24:49 +0100

    updates

---
+++ API/Models/BlobModel.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class BlobModel
+    {
+        public string? Name { get; set; }
+        public DateTimeOffset? CreatedOn { get; set; }
+        public string? URL { get; set; }
+    }
+}
